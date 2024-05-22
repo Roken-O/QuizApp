503b61d80b9719844c2584cc06f741e2b74b51ce
Roken-O <roken.othman91@gmail.com>
2024-05-22 18:20:40 +0200

    cards and JSON araay

---
+++ script.js
@@ -0,0 +1,52 @@
+let questions = [
+  {
+    question: "Welches Tier miaut?",
+    answers: ["Hund", "Katze", "Kuh", "Vogel"],
+    correctAnswer: "Katze",
+  },
+  {
+    question: "Wie viele Beine hat eine Spinne?",
+    answers: ["6", "8", "10", "12"],
+    correctAnswer: "8",
+  },
+  {
+    question: "Welches dieser Tiere lebt im Wasser?",
+    answers: ["Frosch", "Känguru", "Adler", "Elefant"],
+    correctAnswer: "Frosch",
+  },
+  {
+    question: "Was isst ein Panda am liebsten?",
+    answers: ["Bambus", "Fleisch", "Obst", "Blätter"],
+    correctAnswer: "Bambus",
+  },
+  {
+    question: "Welches dieser Tiere kann fliegen?",
+    answers: ["Hund", "Katze", "Fisch", "Vogel"],
+    correctAnswer: "Vogel",
+  },
+  {
+    question: "Welches Tier miaut?",
+    answers: ["Hund", "Katze", "Kuh", "Vogel"],
+    correctAnswer: "Katze",
+  },
+  {
+    question: "Wie viele Beine hat eine Spinne?",
+    answers: ["6", "8", "10", "12"],
+    correctAnswer: "8",
+  },
+  {
+    question: "Welches dieser Tiere lebt im Wasser?",
+    answers: ["Frosch", "Känguru", "Adler", "Elefant"],
+    correctAnswer: "Frosch",
+  },
+  {
+    question: "Was isst ein Panda am liebsten?",
+    answers: ["Bambus", "Fleisch", "Obst", "Blätter"],
+    correctAnswer: "Bambus",
+  },
+  {
+    question: "Welches dieser Tiere kann fliegen?",
+    answers: ["Hund", "Katze", "Fisch", "Vogel"],
+    correctAnswer: "Vogel",
+  },
+];
